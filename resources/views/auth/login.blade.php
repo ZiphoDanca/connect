5f2d8e0e71b5b26dbf46f52c080fd78c5fe358f0
Zipho Danca <zipho.dancah@gmail.com>
2017-11-16 16:29:47 +0200

    intergrating the template

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,80 @@
+@extends('welcome')
+
+@section('content')
+    <section class="panel b-green">
+        <article class="panel__wrapper">
+            <div class="panel__content">
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="home-content">
+                <div class="heading">
+                    <h4>Login</h4>
+                </div>
+
+                <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label" style="color:white">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label" style="color:white">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                                        <h5 style="color:white">
+                                        Remember Me
+                                        </h5>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-8 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Login
+                                </button>
+
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    Forgot Your Password?
+                                </a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+            </div>
+        </article>
+    </section>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,262 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Stacked - Free CSS Template</title>
+<!--
+Stacked Template
+http://www.templatemo.com/tm-505-stacked
+-->
+        <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
 
-        <title>Laravel</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <nav class="nav">
+          <div class="burger">
+            <div class="burger__patty"></div>
+          </div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+          <ul class="nav__list">
+            <li class="nav__item">
+              <a href="{{ url('homepage') }}" class="nav__link c-blue"><img src="img/home-icon.png" alt=""></a>
+            </li>
+            <li class="nav__item">
+              <a href="#2" class="nav__link c-yellow scrolly"><img src="img/about-icon.png" alt=""></a>
+            </li>
+            <li class="nav__item">
+              <a href="#3" class="nav__link c-red"><img src="img/projects-icon.png" alt=""></a>
+            </li>
+            <li class="nav__item">
+              <a href="#4" class="nav__link c-green"><img src="img/contact-icon.png" alt=""></a>
+            </li>
+          </ul>
+        </nav>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+
+            @yield('content')
+
+        {{--<section class="panel b-yellow" id="2">--}}
+          {{--<article class="panel__wrapper">--}}
+            {{--<div class="panel__content">--}}
+              {{--<div class="container">--}}
+                {{--<div class="row">--}}
+                  {{--<div class="col-md-8 col-md-offset-2">--}}
+                    {{--<div class="about-content">--}}
+                      {{--<div class="heading">--}}
+                        {{--<h4>About us</h4>--}}
+                      {{--</div>--}}
+                      {{--<div class="row">--}}
+                        {{--<div class="col-md-12">--}}
+                          {{--<div class="about-box-content">--}}
+                            {{--<img src="img/about-image.png" alt="">--}}
+                          {{--</div>--}}
+                        {{--</div>--}}
+                        {{--<div class="col-md-7 col-md-offset-5">--}}
+                          {{--<div class="about-box-text">--}}
+                            {{--<h4>Curabitur varius sapien</h4>--}}
+                            {{--<p>Nulla vel accumsan dui. Aliquam tincidunt elit elit, nec iaculis neque interdum dictum. Donec venenatis quam enim, eu mattis arcu rhoncus id. Ut ac urna tellus.</p>--}}
+                            {{--<div class="primary-button">--}}
+                              {{--<a href="#3">Discover More</a>--}}
+                            {{--</div>--}}
+                          {{--</div>--}}
+                        {{--</div>--}}
+                      {{--</div>--}}
+                    {{--</div>--}}
+                  {{--</div>--}}
+                {{--</div>--}}
+              {{--</div>--}}
+            {{--</div>--}}
+          {{--</article>--}}
+        {{--</section>--}}
+        {{--<section class="panel b-red" id="3">--}}
+          {{--<article class="panel__wrapper">--}}
+            {{--<div class="panel__content">--}}
+              {{--<div class="container">--}}
+                {{--<div class="row">--}}
+                  {{--<div class="col-md-8 col-md-offset-2">--}}
+                    {{--<div class="projects-content">--}}
+                      {{--<div class="heading">--}}
+                        {{--<h4>Recent Projects</h4>--}}
+                      {{--</div>--}}
+                      {{--<div class="row">--}}
+                        {{--<div class="col-md-12">--}}
+                          {{--<div class="owl-carousel owl-theme projects-container">--}}
+                            {{--<div>--}}
+                              {{--<div class="project-item">--}}
+                                {{--<a href="img/project-item-01.jpg" data-lightbox="image-1"><img src="img/project-item-01.jpg" alt=""></a>--}}
+                                {{--<div class="text-content">--}}
+                                  {{--<h4>Best Design</h4>--}}
+                                  {{--<p>Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.</p>--}}
+                                  {{--<div class="primary-button">--}}
+                                    {{--<a href="#">Discover More</a>--}}
+                                  {{--</div>--}}
+                                {{--</div>--}}
+                              {{--</div>--}}
+                            {{--</div>--}}
+                            {{--<div>--}}
+                              {{--<div class="project-item">--}}
+                                {{--<a href="img/project-item-02.jpg" data-lightbox="image-1"><img src="img/project-item-02.jpg" alt=""></a>--}}
+                                {{--<div class="text-content">--}}
+                                  {{--<h4>Creative Pen</h4>--}}
+                                  {{--<p>Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.</p>--}}
+                                  {{--<div class="primary-button">--}}
+                                    {{--<a href="#">Discover More</a>--}}
+                                  {{--</div>--}}
+                                {{--</div>--}}
+                              {{--</div>--}}
+                            {{--</div>--}}
+                            {{--<div>--}}
+                              {{--<div class="project-item">--}}
+                                {{--<a href="img/project-item-03.jpg" data-lightbox="image-1"><img src="img/project-item-03.jpg" alt=""></a>--}}
+                                {{--<div class="text-content">--}}
+                                  {{--<h4>Nice Capture</h4>--}}
+                                  {{--<p>Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.</p>--}}
+                                  {{--<div class="primary-button">--}}
+                                    {{--<a href="#">Discover More</a>--}}
+                                  {{--</div>--}}
+                                {{--</div>--}}
+                              {{--</div>--}}
+                            {{--</div>--}}
+                            {{--<div>--}}
+                              {{--<div class="project-item">--}}
+                                {{--<a href="img/project-item-04.jpg" data-lightbox="image-1"><img src="img/project-item-04.jpg" alt=""></a>--}}
+                                {{--<div class="text-content">--}}
+                                  {{--<h4>This Moment</h4>--}}
+                                  {{--<p>Lorem ipsum dolor sit amet, adipis scing elit etiam sit amet ante vehicula, aliquam mauris in, luctus neque.</p>--}}
+                                  {{--<div class="primary-button">--}}
+                                    {{--<a href="#">Discover More</a>--}}
+                                  {{--</div>--}}
+                                {{--</div>--}}
+                              {{--</div>--}}
+                            {{--</div>--}}
+                            {{--<div>--}}
+                              {{--<div class="project-item">--}}
+                                {{--<a href="img/project-item-05.jpg" data-lightbox="image-1"><img src="img/project-item-05.jpg" alt=""></a>--}}
+                                {{--<div class="text-content">--}}
+                                  {{--<h4>Artwork</h4>--}}
+                                  {{--<p>Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.</p>--}}
+                                  {{--<div class="primary-button">--}}
+                                    {{--<a href="#">Discover More</a>--}}
+                                  {{--</div>--}}
+                                {{--</div>--}}
+                              {{--</div>--}}
+                            {{--</div>--}}
+                            {{--<div>--}}
+                              {{--<div class="project-item">--}}
+                                {{--<a href="img/project-item-06.jpg" data-lightbox="image-1"><img src="img/project-item-06.jpg" alt=""></a>--}}
+                                {{--<div class="text-content">--}}
+                                  {{--<h4>Item Sixth</h4>--}}
+                                  {{--<p>Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.</p>--}}
+                                  {{--<div class="primary-button">--}}
+                                    {{--<a href="#">Discover More</a>--}}
+                                  {{--</div>--}}
+                                {{--</div>--}}
+                              {{--</div>--}}
+                            {{--</div>--}}
+                            {{--<div>--}}
+                              {{--<div class="project-item">--}}
+                                {{--<a href="img/project-item-07.jpg" data-lightbox="image-1"><img src="img/project-item-07.jpg" alt=""></a>--}}
+                                {{--<div class="text-content">--}}
+                                  {{--<h4>Last Item</h4>--}}
+                                  {{--<p>Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.</p>--}}
+                                  {{--<div class="primary-button">--}}
+                                    {{--<a href="#">Discover More</a>--}}
+                                  {{--</div>--}}
+                                {{--</div>--}}
+                              {{--</div>--}}
+                            {{--</div>--}}
+                          {{--</div>--}}
+                        {{--</div>--}}
+                      {{--</div>--}}
+                    {{--</div>--}}
+                  {{--</div>--}}
+                {{--</div>--}}
+              {{--</div>--}}
+            {{--</div>--}}
+          {{--</article>--}}
+        {{--</section>--}}
+        <section class="panel b-green" id="4">
+          <article class="panel__wrapper">
+            <div class="panel__content">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-8 col-md-offset-2">
+                    <div class="contact-content">
+                      <div class="heading">
+                        <h4>Contact us</h4>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-8">
+                          <div class="contat-form">
+                            <form id="contact" action="" method="post">
+                              <fieldset>
+                                <input name="name" type="text" class="form-control" id="name" placeholder="Your Name" required="">
+                              </fieldset>
+                              <fieldset>
+                                <input name="email" type="email" class="form-control" id="email" placeholder="Email" required="">
+                              </fieldset>
+                              <fieldset>
+                                <textarea name="message" rows="6" class="form-control" id="message" placeholder="Message" required=""></textarea>
+                              </fieldset>
+                              <fieldset>
+                                <button type="submit" id="form-submit" class="btn">Send Message</button>
+                              </fieldset>
+                            </form>
+                          </div>
+                        </div>
+                        <div class="col-md-4">
+                          <div class="more-info">
+                            <p>Lorem ipsum dolor , consectetur adipiscing elit. Nunc purus ligula, ullamcorper id velit id, vestibulum auc sapien. Sed quis mauris eget sem imperdiet rhoncus.<br><br>
+                            <em>3344 Etiam mauris erat,
+								<br>Vestibulum eu augue nec, 10550</em></p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-12">
+                    <div class="footer">
+                      <p>Copyright &copy; 2017 Your Company
+                      | Designed by <a href="http://www.templatemo.com" target="_parent">templatemo</a></p>
+                    </div>
+                  </div>
                 </div>
+              </div>
             </div>
-        </div>
+          </article>
+        </section>
+      
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
     </body>
 </html>
